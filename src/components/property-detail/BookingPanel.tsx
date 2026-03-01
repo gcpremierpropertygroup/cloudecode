1c94cf12dae7caf593674bf851fa09b1aef7c7b0
GC Premier Property Group <gcpremierpropertygroup@gmail.com>
2026-03-01 11:31:21 -0600

    Revert accidental changes to booking panel message

--- src/components/property-detail/BookingPanel.tsx
+++ src/components/property-detail/BookingPanel.tsx
@@ -188,10 +188,7 @@ export default function BookingPanel({
         </a>
 
         <p className="text-xs text-white/30 text-center mt-3">
-          {t("propertyDetail.redirectToAirbnb")}{" "}
-          <a href="/contact" className="text-gold/60 hover:text-gold underline transition-colors">
-            {t("contact.title")}
-          </a>
+          {t("propertyDetail.redirectToAirbnb")}
         </p>
       </div>
     );
