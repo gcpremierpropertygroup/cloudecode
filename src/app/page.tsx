52b893330658a8f21d40f0124c8df644e2f71afd
GC Premier Property Group <gcpremierpropertygroup@gmail.com>
2026-03-13 00:54:55 -0500

    feat: add StatsSection to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,43 @@
 import HeroSection from "@/components/home/HeroSection";
 import AboutSection from "@/components/home/AboutSection";
+import HowItWorks from "@/components/home/HowItWorks";
+import FeaturedProperties from "@/components/home/FeaturedProperties";
+import StatsSection from "@/components/home/StatsSection";
+import ReviewsSection from "@/components/home/ReviewsSection";
 import DirectBookingBanner from "@/components/home/DirectBookingBanner";
-import CTABanner from "@/components/home/CTABanner";
 import ContactSection from "@/components/home/ContactSection";
+import { getGuestyService } from "@/lib/guesty";
+import { getDisplayPrices } from "@/lib/admin/prices";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const service = getGuestyService();
+  const [rawProperties, displayPrices] = await Promise.all([
+    service.getListings(),
+    getDisplayPrices(),
+  ]);
+
+  const properties = rawProperties.map((property) => {
+    if (displayPrices[property.id]) {
+      return {
+        ...property,
+        pricing: {
+          ...property.pricing,
+          baseNightlyRate: displayPrices[property.id],
+        },
+      };
+    }
+    return property;
+  });
+
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <StatsSection />
+      <HowItWorks />
+      <FeaturedProperties properties={properties} />
+      <ReviewsSection />
       <DirectBookingBanner />
-      <CTABanner />
       <ContactSection />
     </>
   );
